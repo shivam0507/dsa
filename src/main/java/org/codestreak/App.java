e5654f8981d9bbac248642518184af8db396563c
shivam gupta <guptashivam0507@gmail.com>
2021-10-31 10:49:18 +0530

    project setup

---
+++ src/main/java/org/codestreak/App.java
@@ -0,0 +1,13 @@
+package org.codestreak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
